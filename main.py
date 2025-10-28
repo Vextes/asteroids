1bda5de98ef2c46122eebf62d59f3737f18f9009
vextes <sjfrang@gmail.com>
2025-10-28 12:04:34 -0500

    Player can be seen on screen

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -12,15 +13,20 @@ def main():
     size = (SCREEN_WIDTH, SCREEN_HEIGHT)
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
 
     #Gameloop
-    while True:
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
-        pygame.Surface.fill(screen,(0,0,0))
+                running = False
+            
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
 
 
